80441c78b30f53ab4cb693bee4546779d4b31cac
Carl Poole <carl@ionic.io>
2021-01-27 14:36:05 -0600

    added form

--- src/pages/AddItem.tsx
+++ src/pages/AddItem.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './AddItem.css';
+import { Controller, useForm } from 'react-hook-form';
 
 const AddItem: React.FC = () => {
+  const { control, handleSubmit } = useForm();
+
+  const addItem = (data : any) => {
+    alert("creating a new todo with: " + data);
+  }
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,13 +17,21 @@ const AddItem: React.FC = () => {
           <IonTitle>Add Todo</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Add</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Add Todo" />
+      <IonContent className="ion-padding">
+        <form onSubmit={handleSubmit(addItem)}>
+          <IonItem>
+            <IonLabel position="floating">Text</IonLabel>
+            <Controller
+              as={<IonInput/>}
+              name="todo"
+              control={control}
+              onChangeName="onIonChange"
+            />
+          </IonItem>
+          <IonButton expand="block" type="submit" className="ion-margin-top">
+            Add
+          </IonButton>
+        </form>
       </IonContent>
     </IonPage>
   );
